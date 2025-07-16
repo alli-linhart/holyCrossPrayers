37142040a08537c280f550a427246bd261779af3
alli-linhart <ajacob22@nd.edu>
2025-07-16 17:58:58 -0400

    Delete things we don't need

--- app/index.js
+++
@@ -1,9 +0,0 @@
-import { Button, Text, View } from 'react-native';
-export default function Home({ navigation }) {
-  return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>Home Screen</Text>
-      <Button title="Open Drawer" onPress={() => navigation.DrawerNavigator()} />
-    </View>
-  );
-}
